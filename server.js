155b641c6a254f439b904debadd76794d9e54720
acufarah <farahkhanu@gmail.com>
2021-03-06 20:31:27 -0500

    Set up api endpoints

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const connectDB= require('./config/db');
 
 const app = express();
 
-//connect database
+//Connect database
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define routes
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
